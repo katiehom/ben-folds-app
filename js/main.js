2d4ab0f20d8be9496cae40f81ef29b0e60f4e8f4
Katie Hom <schaka01@gmail.com>
2022-06-05 10:57:39 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.querySelector('#getButton').addEventListener('click', apiRequest)
+
+async function apiRequest() {
+    const songTitle = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://ben-folds-api.herokuapp.com/api/${songTitle}`)
+        const data = await response.json()
+        console.log(data)
+
+        document.getElementById('songTitle').innerText = data.title
+        document.getElementById('artist').innerText = data.artist
+        document.getElementById('album').innerText = data.album
+        document.getElementById('year').innerText = data.year
+        document.getElementById('albumImage').src = data.image
+        document.getElementById('albumCaption').innerText = data.album
+    } catch(error) {
+        console.log(error)
+    }
+}
