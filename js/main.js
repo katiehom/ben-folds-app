324f3636eda370069190f8d77d7fd65e258ae36b
Katie Hom <schaka01@gmail.com>
2022-11-15 12:18:41 -0700

    Change Heroku API link to Railway

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest() {
     const songTitle = document.querySelector('input').value
     try {
-        const response = await fetch(`https://ben-folds-api.herokuapp.com/api/${songTitle}`)
+        const response = await fetch(`https://ben-folds-api.up.railway.app/api/${songTitle}`)
         const data = await response.json()
         console.log(data)
 
